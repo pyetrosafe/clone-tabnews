d3884802416bcfd505d8b2703f3da62969647e85
Pyetro Costa <pyetrosafe@gmail.com>
2026-04-20 20:44:58 -0300

    Primeiro commit e o desafio de impactar alguém!

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+function Home() {
+    return <div>
+        <h1>
+            No começo era apenas diversão...
+        </h1>
+        <p>
+            ... Interesse, paixão. Mas, sem esperar, sem planejar, tudo mostrou o que realmente estava acontecendo.
+        </p>
+        <p>
+            Quem esteve comigo no sufoco, me ajudou a levantar e meu deu: a mão, um abraço, um sorriso, um conselho, uma palavra de conforto, uma palavra de esperança, uma palavra de fé, uma palavra de amor, uma palavra de carinho, uma palavra de respeito, uma palavra de admiração, uma palavra de apoio, uma palavra de incentivo, uma palavra de motivação, uma palavra de inspiração, uma palavra de força, uma palavra de coragem, uma palavra de determinação, uma palavra de superação, uma palavra de vitória, uma palavra de gratidão, uma palavra de reconhecimento.
+        </p>
+        <p>
+            Essa pessoa sempre vai estar em meu coração, em minhas orações e eu sou eternamente grato por tudo.
+        </p>
+        <p>
+            Obrigado por estar comigo desde o começo. Te amo...
+        </p>
+        <p>
+            Se você sente, algo parecido, pelo menos um pouquinho que seja, me dê um sorisso, um abraço e um beijo. ❤️
+        </p>
+
+    </div>
+}
+
+export default Home;
